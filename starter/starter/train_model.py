503a241756034f77963caf074f27381744999926
hitoshikumagai <78469337+hitoshikumagai@users.noreply.github.com>
2022-02-06 19:55:04 +0900

    1st commit

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,18 @@
 # Script to train machine learning model.
-
+import pickle
+import pandas as pd
+import numpy as np
+from ml.data import process_data
+from ml.model import train_model,compute_model_metrics,inference
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestClassifier as Model
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data = pd.read_csv('../data/census.csv') 
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.2)
 
 cat_features = [
     "workclass",
@@ -23,6 +28,13 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=True
+)
 
 # Train and save a model.
+model=Model(random_state=42)
+model = train_model(X_train, y_train, model)
+preds = inference(model,X_test)
+precision, recall, fbeta =compute_model_metrics(y_test, preds)

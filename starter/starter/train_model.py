0a6440095b573406d9d55addddf9760a93578c4b
hitoshikumagai <78469337+hitoshikumagai@users.noreply.github.com>
2022-02-08 07:18:57 +0900

    feat: Change model_train.py to read the configuration information and build the model.

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,4 +1,11 @@
+"""
+
+Author: 
+Date: 
+"""
+
 # Script to train machine learning model.
+import yaml
 import pickle
 import pandas as pd
 import numpy as np
@@ -8,10 +15,14 @@ from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 
+with open('config.yaml','rb') as yml:
+    config = yaml.safe_load(yml)
+
 # Add code to load in the data.
-data = pd.read_csv('../data/census.csv') 
+data = pd.read_csv(config['main']['data_pth']) 
+
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.2)
+train, test = train_test_split(data, test_size=config['model']['test_size'])
 
 cat_features = [
     "workclass",
@@ -24,21 +35,20 @@ cat_features = [
     "native-country",
 ]
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train, categorical_features=cat_features, label=config['model']['label'], training=True
 )
 
 # Process the test data with the process_data function.
 X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features,encoder=encoder, lb=lb,label="salary", training=False
+    test, categorical_features=cat_features,encoder=encoder, lb=lb,label=config['model']['label'], training=False
 )
 
 # Train and save a model.
 model = train_model(X_train, y_train)
 
-file = '../model/trained_model.pkl'
+file = config['main']["model_pth"]
 pickle.dump(model, open(file, 'wb'))
 
 preds = inference(model,X_test)
 precision, recall, fbeta =compute_model_metrics(y_test, preds)
 print(f"Precision:{precision:.3f}, Recall:{recall:.3f}, F1 value:{fbeta:.3f}")
-

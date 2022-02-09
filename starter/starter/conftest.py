83937ff75aae347708600a1ce399c14a22d85d26
hitoshikumagai <78469337+hitoshikumagai@users.noreply.github.com>
2022-02-09 21:27:02 +0900

    bug: Delete yaml config in conftest.py

--- starter/starter/conftest.py
+++ starter/starter/conftest.py
@@ -4,20 +4,15 @@
 """
 import pandas as pd
 import pytest 
-import yaml
 import logging
 
 
 @pytest.fixture(scope='session')
 def data():
     """[summary]
-
     Returns:
         [type]: [description]
     """
-    with open('config.yaml','rb') as yml:
-        config = yaml.safe_load(yml)
-    
-    data = pd.read_csv(config['main']['data_pth'])
+    data = pd.read_csv('../data/census_cleaned.csv')
     
     return data

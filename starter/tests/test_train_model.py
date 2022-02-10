84476a942e25d13cf1b1ec2f8671939714e8f621
hitoshikumagai <78469337+hitoshikumagai@users.noreply.github.com>
2022-02-10 21:02:07 +0900

    feat:Add configlation in yaml to select test environment

--- starter/tests/test_train_model.py
+++ starter/tests/test_train_model.py
@@ -7,10 +7,13 @@ import pandas as pd
 import pytest
 from pytest import fixture
 import logging
+import yaml
 
+with open('config.yaml','rb') as yml:
+    config = yaml.safe_load(yml)
 
 logging.basicConfig(
-    filename='../logs/census_train.log',
+    filename=config['main']["online"]['data_pth'],
     level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
@@ -21,7 +24,7 @@ def data():
     Returns:
         [type]: [description]
     """
-    data = pd.read_csv('../data/census_cleaned.csv')
+    data = pd.read_csv(config['main']["online"]['data_pth'])
     
     return data
 
